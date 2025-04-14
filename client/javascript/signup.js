6f44efc52cb95b38cfc29d9f6292df3afdffac1c
Yan <dragonicle57@gmail.com>
2025-04-14 12:36:58 +0800

    Refactor: Move inline JS to external file and update stylesheet link

---
+++ client/javascript/signup.js
@@ -0,0 +1,8 @@
+document.getElementById("signupForm").addEventListener("submit", function (e) {
+  const password = document.getElementById("password").value;
+  const confirm = document.getElementById("confirmPassword").value;
+  if (password !== confirm) {
+    e.preventDefault();
+    alert("Passwords do not match!");
+  }
+});
