776ea9f715ff775b9abd22852103862e1f8ea5f6
JillStingray1 <jerryhouau@gmail.com>
2025-04-19 11:49:03 +0800

    defined required functions and added basic documentation

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,1 +1,27 @@
-# TODO : add middleware for user auth on all routes except login and signup. also add encryption for passwords and 
+# TODO : add middleware for user auth on all routes except login and signup. also add encryption for passwords and
+
+
+def validate_password(password):
+    """
+    _summary_
+
+    Args:
+        password (string): the password sent to the srever
+
+    Returns:
+        boolean: describes whether the password fits the requirements and is safe to use in queries
+    """
+    return True
+
+
+def validate_username(username):
+    """
+    _summary_
+
+    Args:
+        username (string): the username set to the server
+
+    Returns:
+        boolean: describes whether the username fits the requirements and is safe to use in queries
+    """
+    return True
