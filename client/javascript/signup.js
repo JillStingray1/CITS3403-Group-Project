863b1885c1a11ac981390de5e4d598cc92283abd
Looking Cool, Jill <87292700+JillStingray1@users.noreply.github.com>
2025-04-20 23:13:58 +0800

    Delete client/javascript/signup.js
    The JS is not functional, and does not provide sufficient validation for our purposes.

--- client/javascript/signup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("signupForm").addEventListener("submit", function (e) {
-  const password = document.getElementById("password").value;
-  const confirm = document.getElementById("confirmPassword").value;
-  if (password !== confirm) {
-    e.preventDefault();
-    alert("Passwords do not match!");
-  }
-});
