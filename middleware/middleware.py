a2b44045ab1f0a80d22df927ad4fe7069e1e8de1
JillStingray1 <jerryhouau@gmail.com>
2025-04-20 17:18:51 +0800

    added a password function

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -4,21 +4,27 @@ import re
 
 def validate_password(password: str) -> bool:
     """
-    _summary_
-
+    Checks if a inputted username is valid. Passwords should only contain all
+    alphanumeric characters, and special characters !#+:=.?
     Args:
         password (string): the password sent to the srever
 
     Returns:
         boolean: describes whether the password fits the requirements and is safe to use in queries
     """
-    return True
+    if len(password) < 5 or len(password) > 35:
+        return False
+    san_password = re.search("[A-Za-z0-9!#+:=.?]+", password)
+    if san_password:
+        return san_password.group() == password
+    return False
 
 
 def validate_username(username: str) -> bool:
     """
     Checks if a inputted username is valid. Username should only contain
-    alphanumeric characters (excluding decimals) and underscores.
+    alphanumeric characters (excluding decimals) and underscores, and must be
+    at least 3 characters long.
 
     Args:
         username (string): the username set to the server
@@ -26,7 +32,7 @@ def validate_username(username: str) -> bool:
     Returns:
         boolean: describes whether the username fits the requirements and is safe to use in queries
     """
-    if len(username) < 3:
+    if len(username) < 3 or len(username) > 25:
         return False
     san_username = re.search("[A-Za-z0-9_]+", username)
     if san_username:
