c76e1b24efdff5977d073f41644c0053abdaf955
JillStingray1 <jerryhouau@gmail.com>
2025-04-20 17:02:43 +0800

    added tests for minimum length of usre name

--- test/test_validation.py
+++ test/test_validation.py
@@ -9,11 +9,13 @@ class TestValidation(unittest.TestCase):
         self.assertEqual(middleware.validate_password("; drop table *;"), False)
 
     def test_username_validation(self):
+        self.assertEqual(middleware.validate_username(""), False)
+        self.assertEqual(middleware.validate_username("a"), False)
+        self.assertEqual(middleware.validate_username("aa"), False)
+        self.assertEqual(middleware.validate_username("aaa"), True)
         self.assertEqual(middleware.validate_username("abcd8"), True)
         self.assertEqual(middleware.validate_username("Jill_Stingray"), True)
         self.assertEqual(middleware.validate_username("ACcd.8"), False)
         self.assertEqual(middleware.validate_username("ACcd?8"), False)
         self.assertEqual(middleware.validate_username("ACcd!8"), False)
-        self.assertEqual(middleware.validate_username("ACcd.8"), False)
-
         self.assertEqual(middleware.validate_username("; drop table *;"), False)
