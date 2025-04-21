70ed92551cb3ea6b97a4ca57ced3ec9a056e6eed
JillStingray1 <jerryhouau@gmail.com>
2025-04-21 11:11:37 +0800

    moved password validation to tools

--- test/test_validation.py
+++ test/test_validation.py
@@ -1,5 +1,5 @@
 import unittest
-from middleware import middleware
+from server import tools
 
 
 class TestValidation(unittest.TestCase):
@@ -7,37 +7,37 @@ class TestValidation(unittest.TestCase):
     def test_password_validation(self):
         # test cases for not meeting the length requirements
         self.assertEqual(
-            middleware.validate_password("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+            tools.validate_password("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
             False,
         )
         self.assertEqual(
-            middleware.validate_password("A"),
+            tools.validate_password("A"),
             False,
         )
 
         # test cases for rejecting special characters
-        self.assertEqual(middleware.validate_password(";::::;;;:"), False)
-        self.assertEqual(middleware.validate_password("; drop table *;"), False)
+        self.assertEqual(tools.validate_password(";::::;;;:"), False)
+        self.assertEqual(tools.validate_password("; drop table *;"), False)
 
         # good passwords tests
-        self.assertEqual(middleware.validate_password("abcd8"), True)
-        self.assertEqual(middleware.validate_password("!#+:=.?"), True)
+        self.assertEqual(tools.validate_password("abcd8"), True)
+        self.assertEqual(tools.validate_password("!#+:=.?"), True)
 
     def test_username_validation(self):
         # test cases for not meeting the length requirments
-        self.assertEqual(middleware.validate_username("aa"), False)
+        self.assertEqual(tools.validate_username("aa"), False)
         self.assertEqual(
-            middleware.validate_password("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+            tools.validate_password("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
             False,
         )
 
         # test cases for rejecting special characters
-        self.assertEqual(middleware.validate_username("ACcd.8"), False)
-        self.assertEqual(middleware.validate_username("ACcd?8"), False)
-        self.assertEqual(middleware.validate_username("ACcd!8"), False)
-        self.assertEqual(middleware.validate_username("; drop table *;"), False)
+        self.assertEqual(tools.validate_username("ACcd.8"), False)
+        self.assertEqual(tools.validate_username("ACcd?8"), False)
+        self.assertEqual(tools.validate_username("ACcd!8"), False)
+        self.assertEqual(tools.validate_username("; drop table *;"), False)
 
         # good usernames tests
-        self.assertEqual(middleware.validate_username("aaa"), True)
-        self.assertEqual(middleware.validate_username("abcd8"), True)
-        self.assertEqual(middleware.validate_username("Jill_Stingray"), True)
+        self.assertEqual(tools.validate_username("aaa"), True)
+        self.assertEqual(tools.validate_username("abcd8"), True)
+        self.assertEqual(tools.validate_username("Jill_Stingray"), True)
