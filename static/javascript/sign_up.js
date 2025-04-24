f9e0817875b9df40fa9a787409afe8d05e93b9cb
JillStingray1 <jerryhouau@gmail.com>
2025-04-24 12:11:59 +0800

    completed clientside validation of form

---
+++ static/javascript/sign_up.js
@@ -0,0 +1,85 @@
+
+/**
+ * Takes in the values in the sign-up form and sends it to /user,
+ * which is the route that queries the database.
+ * 
+ * 
+ */
+function create_user() {
+    let username = document.forms["signup"]["username"].value
+    let password = document.forms["signup"]["password"].value
+    
+    let confirm_password = document.forms["signup"]["confirm_password"].value
+    let username_validity = is_username_valid(username)
+    let password_validity = is_password_valid(password)
+    if (username_validity && password_validity) {
+        return false;
+    }
+}
+
+/**
+ * Validates user inputed usernames, which can only contain alphanumeric 
+ * characters and underscores
+ * 
+ * @param {string} username the username string from user input
+ * @returns boolean that verifies user input
+ */
+function is_username_valid(username) {
+    let santinized_username = username.match(new RegExp("[A-Za-z0-9_]+"));
+    if (username.length < 3 && username.length > 25 ) {
+        document.getElementById("username_warning").textContent 
+            = "Username must be between 3 and 25 characters.";
+        return false
+    }
+    if (santinized_username[0] != username){
+        document.getElementById("username_warning").textContent 
+            = "Username must only contain alphanumeric characters and underscores.";
+        return false
+    }
+    return true;
+}
+
+/**
+ * Validates user inputed passwords, Password must only contain alphanumeric 
+ * characters and the following special characters !#+:=.?
+ * 
+ * @param {string} password the password string from user input
+ * @returns boolean that verifies user input
+ */
+function is_password_valid(password) {
+    let santinized_password = password.match(new RegExp("[A-Za-z0-9!#+:=.?]+"));
+    if (password.length < 5 && password.length > 35 ) {
+        document.getElementById("pasword_warning").textContent 
+            = "Password must be between 5 and 35 characters.";
+        return false
+    }
+    if (santinized_password[0] != password){
+        document.getElementById("password_warning").textContent 
+            = "Password must only contain alphanumeric characters and the following special characters !#+:=.?";
+        return false
+    }
+    return true;
+
+}
+
+function password_matching(password, confirm_password) {
+    if (password != confirm_password) {
+        document.getElementById("confirm_warning").textContent 
+            = "Provided passwords must be identical."
+        return 
+    }
+    return true
+}
+
+function post_user(username, password) {
+    fetch()
+}
+
+window.onload = function ()  {
+    let signup_form = document.getElementById("signup")
+    signup_form.addEventListener('submit', event => {
+        event.preventDefault();
+        return create_user();
+    })
+}
+
