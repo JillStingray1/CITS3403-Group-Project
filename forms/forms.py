b01222cc84b85fb2f4bd06e9773eb584ea0df05d
JillStingray1 <jerryhouau@gmail.com>
2025-04-26 21:03:12 +0800

    added regex validation

--- forms/forms.py
+++ forms/forms.py
@@ -1,16 +1,39 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 
 
 class SignUpForm(FlaskForm):
-    fullname = StringField("Full Name", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=5)])
+    username = StringField(
+        "Username",
+        validators=[
+            DataRequired(),
+            Length(
+                min=3, max=25, message="Username must be between 3 to 25 characters"
+            ),
+            Regexp(
+                "[A-Za-z0-9_]+",
+                message="Username must contain only alphanumeric characters and _",
+            ),
+        ],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            Length(
+                min=5, max=35, message="Password must be between 5 to 35 characters"
+            ),
+            Regexp(
+                "[A-Za-z0-9!#+:=.?]+",
+                message="Password must contain only alphanumeric characters and the following special characters !#+:=.?",
+            ),
+        ],
+    )
     confirm_password = PasswordField(
         "Confirm Password",
         validators=[
             DataRequired(),
-            Length(min=5),
             EqualTo("password", message="Passwords must match."),
         ],
     )
