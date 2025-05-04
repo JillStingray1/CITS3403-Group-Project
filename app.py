a7c0a2778e292667575884ce7b25625c1484db6f
Tylen Chetty <169156149+TDC350@users.noreply.github.com>
2025-05-04 16:02:45 +0800

    Update app.py with Login Manager

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, render_template
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_wtf import CSRFProtect
 from extensions import db
 from dotenv import load_dotenv
+from routes import user_routes, meeting_routes
 import os
+
+from flask_login import LoginManager
+from models import User  # your User model
 
 load_dotenv()
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
+login_manager = LoginManager()
+login_manager.login_view = 'login_user'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 app.config['SECRET_KEY'] = os.getenv('SESSION_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_URI')
+CSRFProtect(app)
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -25,9 +39,9 @@ with app.app_context():
 
 @app.route('/')
 def index():
-    return redirect(url_for('static', filename='index.html'))
+    return render_template("index.html")
 
-from routes import user_routes, meeting_routes
+
 user_routes.init_user_routes(app, db, bcrypt)
 meeting_routes.init_meeting_routes(app, db)
 
