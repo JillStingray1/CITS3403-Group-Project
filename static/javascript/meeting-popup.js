78ca5184ff662fde93b4a687aea495f0e475e1b6
JillStingray1 <jerryhouau@gmail.com>
2025-05-16 10:33:18 +0800

    added a close button on the share code preview

--- static/javascript/meeting-popup.js
+++ static/javascript/meeting-popup.js
@@ -1,9 +1,16 @@
 /**
- * Client side for share code related data.
- * 
  * 
  */
+function hide_popup() {
+    document.getElementById("popup").style.display = "none";
+}
 
+/**
+ * Shows a popup of the meeting's details from the share code to allow 
+ * users to confirm whether they have imported the correct meeting
+ * 
+ * @param {*} e the confirm event
+ */
 function confirm_share_code(e) {
     // Prevents the confirmation popup from disappearing when the dropdown disappears
     e.preventDefault()
@@ -32,12 +39,15 @@ function confirm_share_code(e) {
             }
         })
     } catch (error) {
+        document.getElementById("error").textContent = "There's been an error with geting your meeting";
     }
-
-    
 }
 
+
+
 window.onload = () => {
-    const openBtn = document.getElementById("show-details");
-    openBtn.addEventListener("click", confirm_share_code)
+    const open_btn = document.getElementById("show-details");
+    open_btn.addEventListener("click", confirm_share_code)
+    const close_btn = document.getElementById("close-button");
+    close_btn.addEventListener("click", hide_popup)
 }
